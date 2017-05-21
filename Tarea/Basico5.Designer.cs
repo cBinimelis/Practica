c90b9b00d360f31ea4632fd5285bc6457c363513
Cristofer Binimelis <cristoferbinimelis@gmail.com>
2017-05-21 14:49:50 -0400

    Tercer modulo en proceso

---
+++ Tarea/Basico5.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Tarea
+{
+    partial class Basico5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Basico5));
+            this.btn_cambio = new System.Windows.Forms.Button();
+            this.lbl_NumPregunta = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_restultado = new System.Windows.Forms.TextBox();
+            this.btn_ingreso = new System.Windows.Forms.Button();
+            this.txt_operacion = new System.Windows.Forms.TextBox();
+            this.txt_Num2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Num1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_cambio
+            // 
+            this.btn_cambio.Location = new System.Drawing.Point(12, 204);
+            this.btn_cambio.Name = "btn_cambio";
+            this.btn_cambio.Size = new System.Drawing.Size(126, 32);
+            this.btn_cambio.TabIndex = 26;
+            this.btn_cambio.Text = "Cambiar Pregunta";
+            this.btn_cambio.UseVisualStyleBackColor = true;
+            // 
+            // lbl_NumPregunta
+            // 
+            this.lbl_NumPregunta.AutoSize = true;
+            this.lbl_NumPregunta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.lbl_NumPregunta.Location = new System.Drawing.Point(188, 11);
+            this.lbl_NumPregunta.Name = "lbl_NumPregunta";
+            this.lbl_NumPregunta.Size = new System.Drawing.Size(23, 25);
+            this.lbl_NumPregunta.TabIndex = 25;
+            this.lbl_NumPregunta.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.label2.Location = new System.Drawing.Point(124, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 39);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "=";
+            // 
+            // txt_restultado
+            // 
+            this.txt_restultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.txt_restultado.Location = new System.Drawing.Point(91, 145);
+            this.txt_restultado.MaxLength = 2;
+            this.txt_restultado.Multiline = true;
+            this.txt_restultado.Name = "txt_restultado";
+            this.txt_restultado.Size = new System.Drawing.Size(100, 54);
+            this.txt_restultado.TabIndex = 22;
+            this.txt_restultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_ingreso
+            // 
+            this.btn_ingreso.Location = new System.Drawing.Point(146, 204);
+            this.btn_ingreso.Name = "btn_ingreso";
+            this.btn_ingreso.Size = new System.Drawing.Size(126, 32);
+            this.btn_ingreso.TabIndex = 18;
+            this.btn_ingreso.Text = "Ingresar";
+            this.btn_ingreso.UseVisualStyleBackColor = true;
+            this.btn_ingreso.Click += new System.EventHandler(this.btn_ingreso_Click);
+            // 
+            // txt_operacion
+            // 
+            this.txt_operacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_operacion.Location = new System.Drawing.Point(121, 63);
+            this.txt_operacion.Multiline = true;
+            this.txt_operacion.Name = "txt_operacion";
+            this.txt_operacion.ReadOnly = true;
+            this.txt_operacion.Size = new System.Drawing.Size(45, 38);
+            this.txt_operacion.TabIndex = 23;
+            this.txt_operacion.Text = "+";
+            this.txt_operacion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_Num2
+            // 
+            this.txt_Num2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.txt_Num2.Location = new System.Drawing.Point(172, 48);
+            this.txt_Num2.Multiline = true;
+            this.txt_Num2.Name = "txt_Num2";
+            this.txt_Num2.ReadOnly = true;
+            this.txt_Num2.Size = new System.Drawing.Size(100, 54);
+            this.txt_Num2.TabIndex = 20;
+            this.txt_Num2.Text = "5";
+            this.txt_Num2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(75, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Pregunta #";
+            // 
+            // txt_Num1
+            // 
+            this.txt_Num1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Num1.Location = new System.Drawing.Point(12, 48);
+            this.txt_Num1.Multiline = true;
+            this.txt_Num1.Name = "txt_Num1";
+            this.txt_Num1.ReadOnly = true;
+            this.txt_Num1.Size = new System.Drawing.Size(103, 54);
+            this.txt_Num1.TabIndex = 19;
+            this.txt_Num1.Text = "45";
+            this.txt_Num1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Basico5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 247);
+            this.Controls.Add(this.btn_cambio);
+            this.Controls.Add(this.lbl_NumPregunta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_restultado);
+            this.Controls.Add(this.btn_ingreso);
+            this.Controls.Add(this.txt_operacion);
+            this.Controls.Add(this.txt_Num2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Num1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Basico5";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Basico5";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Basico5_FormClosing);
+            this.Load += new System.EventHandler(this.Basico5_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_cambio;
+        private System.Windows.Forms.Label lbl_NumPregunta;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_restultado;
+        private System.Windows.Forms.Button btn_ingreso;
+        private System.Windows.Forms.TextBox txt_operacion;
+        private System.Windows.Forms.TextBox txt_Num2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Num1;
+    }
+}
